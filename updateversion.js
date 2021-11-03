9ac71233975a86ec6a449b541d6e5b9bce762fbe
devaraj <nsdevaraj@gmail.com>
2021-11-03 20:44:28 +0530

    chore(ver): bump

---
+++ updateversion.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+let packageJSON = require('./package.json');
+
+const buildNumber = process.env.buildNumber || '';
+const branch = process.env.branch || '';
+
+
+
+function getVersion() {
+    let version = packageJSON.version;
+
+    if (buildNumber) {
+        version = version.replace(/-rc\w*/g, '');
+        const versionParts = version.split('.');
+        versionParts[2] = versionParts[2] + (branch == "development" ? '-rc' + buildNumber : '-alpha' + buildNumber);
+        version = versionParts.join('.');
+    }
+    return version;
+}
+
+function updateVersionNumber() {
+    const versionNumber = getVersion();
+    packageJSON.version = versionNumber;
+    fs.writeFileSync('./package.json', JSON.stringify(packageJSON, null, 4));
+}
+
+function logVersionNumber() {
+    console.log(`npm install @visualbi/react-querybuilder@${packageJSON.version}`.blue);
+}
+
+async function main() {
+    updateVersionNumber();
+    logVersionNumber();
+
+}
+
+main();
